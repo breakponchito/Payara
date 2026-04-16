152dc8dd60eec7adb18a2e1c784562cc6c47b3cf
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2026-04-16 11:53:42 -0600

    FISH-13067: adding exception thrown metric

--- appserver/payara-appserver-modules/microprofile/fault-tolerance/src/main/java/fish/payara/microprofile/faulttolerance/FaultToleranceTelemetryMetricsRecorder.java
+++ appserver/payara-appserver-modules/microprofile/fault-tolerance/src/main/java/fish/payara/microprofile/faulttolerance/FaultToleranceTelemetryMetricsRecorder.java
@@ -85,6 +85,9 @@ public class FaultToleranceTelemetryMetricsRecorder {
             longCounter.add(1, Attributes.builder().putAll(Attributes.builder().put(AttributeKey
                             .stringKey("method"), classAndMethodName).build()).put(AttributeKey.stringKey("result"), "valueReturned")
                     .put(AttributeKey.stringKey("fallback"), "notApplied").build());
+            longCounter.add(1, Attributes.builder().putAll(Attributes.builder().put(AttributeKey
+                            .stringKey("method"), classAndMethodName).build()).put(AttributeKey.stringKey("result"), "exceptionThrown")
+                    .put(AttributeKey.stringKey("fallback"), "notApplied").build());
             
         }
         return longCounter;
