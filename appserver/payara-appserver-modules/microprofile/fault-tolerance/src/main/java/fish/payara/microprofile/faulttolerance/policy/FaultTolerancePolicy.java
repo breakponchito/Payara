ef9419eb8124313b45b965d8e23295e99e1a836e
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2026-04-15 19:51:39 -0600

    FISH-13067: validating if the state of FaultToleranceMetrics is maintained during calls

--- appserver/payara-appserver-modules/microprofile/fault-tolerance/src/main/java/fish/payara/microprofile/faulttolerance/policy/FaultTolerancePolicy.java
+++ appserver/payara-appserver-modules/microprofile/fault-tolerance/src/main/java/fish/payara/microprofile/faulttolerance/policy/FaultTolerancePolicy.java
@@ -280,7 +280,8 @@ public final class FaultTolerancePolicy implements Serializable {
             return context.proceed();
         }
         FaultToleranceMethodContext ftmContext = ftmContextSupplier.get();
-        FaultToleranceMetrics metrics = ftmContext.getMetrics().boundTo(ftmContext, this);
+        FaultToleranceMetrics metrics = ftmContext.getMetrics();
+        metrics.boundTo(ftmContext, this);
         try {
             Object res = processAsynchronousStage(ftmContext, metrics);
             if (res instanceof AsyncFuture) {
